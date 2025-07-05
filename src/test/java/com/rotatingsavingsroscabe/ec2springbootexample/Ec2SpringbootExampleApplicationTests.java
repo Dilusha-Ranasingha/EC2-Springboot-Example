78792730a88a249ba9274d033acf101d8b0b96a4
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-07-06 00:08:29 +0530

    initial commit

---
+++ src/test/java/com/rotatingsavingsroscabe/ec2springbootexample/Ec2SpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rotatingsavingsroscabe.ec2springbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2SpringbootExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
