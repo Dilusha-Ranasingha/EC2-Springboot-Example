0b1a6c62e1938c206aec05f2d25c02a4f785d018
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-07-06 05:06:48 +0530

    fix: update CORS origin in TestController to match new production frontend URL

--- src/main/java/com/rotatingsavingsroscabe/ec2springbootexample/controller/TestController.java
+++ src/main/java/com/rotatingsavingsroscabe/ec2springbootexample/controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://ec-2-spring-git-main-dilusha-ranasinghas-projects.vercel.app/")  // Allow frontend running here
+@CrossOrigin(origins = "https://ec-2-spring.vercel.app/")  // Allow frontend running here
 public class TestController {
 
 
