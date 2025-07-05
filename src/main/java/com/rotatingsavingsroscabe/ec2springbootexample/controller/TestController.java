b5b9f53ca645bf78da7507d4edff4c64273549f3
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-07-06 04:41:41 +0530

    feat: enable CORS for TestController to allow frontend access

--- src/main/java/com/rotatingsavingsroscabe/ec2springbootexample/controller/TestController.java
+++ src/main/java/com/rotatingsavingsroscabe/ec2springbootexample/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.rotatingsavingsroscabe.ec2springbootexample.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5174")  // Allow frontend running here
 public class TestController {
 
 
